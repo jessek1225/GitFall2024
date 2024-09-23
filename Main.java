e14d5e8bf0b3765018c1753c110740a641fabadf
jessek1225 <156345205+jessek1225@users.noreply.github.com>
2024-09-23 11:29:18 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
-public class Main{
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-LinkedList<String> studentNames;
-
-public Main(){
-		studentNames = new LinkedList<>();
-		studentNames.add("Peter");
-		studentNames.add("Sierra");
-		studentNames.add("Daron");
-		studentNames.add("");
-		studentNames.add("Abdullah");
-		studentNames.add("Savitha");
-		studentNames.add("");
-		studentNames.add("Alex");
-		studentNames.add("Jake");
-	}
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
 }
